06c642ba33f1db1099610261333c8dea1499c14d
Rishil Shah <114029751+Rishil-student-shah@users.noreply.github.com>
2026-08-28 00:15:35 +0530

    feat: Submodule pointer update and CRM DB button settings for Events

---
+++ Landing Page Work/tiffany-webb-crm/add_events_btn.sql
@@ -0,0 +1,5 @@
+﻿USE tiffany_crm;
+INSERT INTO website_content (page_id, section, key_name, content_value, content_type) VALUES 
+(1, 'events', 'btn_text', 'BOOK TIFFANY', 'text'),
+(1, 'events', 'btn_link', '/work-with-tiffany', 'text')
+ON DUPLICATE KEY UPDATE content_value = VALUES(content_value);
